fc16919ab04c93e63fd062de518fb5ac09b92b20
Tim Douglas <tim.douglas@phocassoftware.com>
2018-12-17 21:56:15 +1100

    Refactor

--- items/index.js
+++ items/index.js
@@ -1,4 +1,6 @@
 const AWS = require("aws-sdk");
+const util = require("../util");
+const uuid = require("../uuid");
 const log = require("../log");
 if (process.env.NODE_ENV === "development") require("dotenv").config();
 
@@ -12,10 +14,46 @@ AWS.config.update({
 
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
-function createItem(request, response, tokens) {}
+function create(request, response, tokens) {
+  util.getPayload(request, function(err, payload) {
+    if (err) {
+      util.respond(
+        500,
+        "ERROR",
+        "Could not create your post. Please try again.",
+        response
+      );
+      return log.error(`[pocket] [Get Payload] ${err}`);
+    } else {
+      let item = {
+        TableName: "pocket-dimension",
+        Item: {
+          id: uuid(),
+          timestamp: new Date().toISOString(),
+          title: payload.title || "",
+          body: payload.body || "",
+          type: payload.type || "note"
+        }
+      };
+      dynamo.put(item, function(err) {
+        if (err) {
+          util.respond(
+            500,
+            "ERROR",
+            "Could not create your post. Please try again.",
+            response
+          );
+          return log.error(`[DynamoDB] [Create Item] ${err}`);
+        } else {
+          util.respond(200, "SUCCESS", "Successfully created post.", response);
+        }
+      });
+    }
+  });
+}
 
 module.exports = {
-  create: createItem,
+  create: create,
   get: "",
   update: "",
   delete: ""
---
+++ util/index.js
@@ -0,0 +1,27 @@
+const util = {
+  getPayload: function parsePayload(request, callback) {
+    let payload = "";
+    request.on("data", function(data) {
+      payload += data;
+    });
+    request.on("error", function(err) {
+      callback(err);
+    });
+    request.on("end", function() {
+      let parsed = JSON.parse(payload);
+      callback(null, parsed);
+    });
+  },
+  respond: function performResponse(code, status, message, response) {
+    response.writeHead(code, { "Content-Type": "application/json" });
+    response.write(
+      JSON.stringify({
+        status: status,
+        result: message
+      })
+    );
+    response.end();
+  }
+};
+
+module.exports = util;
