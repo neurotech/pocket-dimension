41477cfb0f05efc91fb3a4798b07758d7a98b661
Tim Douglas <tim.douglas@phocassoftware.com>
2018-12-17 20:04:31 +1100

    Revert

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === "development") require("dotenv").config();
+require("dotenv").config();
 const web = require("./web");
 
 // Start the web server
--- items/index.js
+++ items/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 if (process.env.NODE_ENV === "development") require("dotenv").config();
 
 const db = require("../db");
@@ -12,6 +13,50 @@ function getItems(request, response, tokens) {
     if (err) console.error(err);
     console.log(res.rows);
     db.end();
+=======
+const AWS = require("aws-sdk");
+const log = require("../log");
+
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.POCKET_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.POCKET_AWS_ACCESS_KEY_SECRET
+  },
+  region: process.env.POCKET_AWS_REGION
+});
+
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+function createItem(request, response, tokens) {
+  let now = new Date();
+  let query = {
+    TableName: "pocket-dimension",
+    Item: {
+      "item-id": Number(1),
+      timestamp: now.toString()
+    }
+  };
+  dynamo.put(query, function(err, data) {
+    if (err) log.error(err);
+
+    console.log(data);
+  });
+}
+
+function getItems(request, response, tokens) {
+  let query = {
+    TableName: "pocket-dimension"
+    // ExpressionAttributeValues: {
+    //   ":t": timestamp
+    // }
+    // KeyConditionExpression: "discordToken = :t"
+  };
+
+  dynamo.scan(query, function(err, data) {
+    if (err || data.Items.length === 0) log.error(err);
+
+    console.log(data);
+>>>>>>> parent of 50c43d4... Bibbly boo
   });
 }
 
