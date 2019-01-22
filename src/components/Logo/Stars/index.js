3e1fe044ee89ee7e837a77bac3e25fa1fbccc577
Tim Douglas <tim.douglas@phocassoftware.com>
2019-01-22 22:54:39 +1100

    Starz fixez

--- src/components/Logo/Stars/index.js
+++ src/components/Logo/Stars/index.js
@@ -4,10 +4,13 @@ const svg = crelns.bind(null, "http://www.w3.org/2000/svg");
 function getRandomInt(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 function getStop(colour) {
   return [
-    svg("stop", { offset: "33%", "stop-color": "rgb(0,0,0)" }),
+    svg("stop", { offset: "40%", "stop-color": "rgb(0,0,0)" }),
     svg("stop", { offset: "90%", "stop-color": `${colour}` }),
     svg("stop", { offset: "95%", "stop-color": `${colour}` }),
     svg("stop", { offset: "100%", "stop-color": "rgb(255,255,255)" })
@@ -15,7 +18,7 @@ function getStop(colour) {
 }
 
 function getAnimate(id, speed) {
-  var delay = getRandomInt(0.5, 3.5);
+  var delay = getRandomFloat(0.5, 3.5);
 
   return svg("animate", {
     attributeType: "XML",
@@ -39,17 +42,14 @@ function getGradient(id, colour) {
 
 function getRect(id, speed) {
   var verticalPosition = getRandomInt(10, 70);
-  var height = getRandomInt(1, 2);
 
   return svg(
     "rect",
     {
       x: "-144",
       y: verticalPosition,
-      rx: "3",
-      ry: "3",
-      width: "128",
-      height: height,
+      width: (1 / speed) * 50,
+      height: 1,
       fill: `url('#gradient-${id}')`
     },
     getAnimate(id, speed)
@@ -79,13 +79,13 @@ module.exports = function createStars() {
     var speedNumber = 1;
 
     if (speed === "slow") {
-      speedNumber = getRandomInt(7, 11);
+      speedNumber = getRandomFloat(7, 9);
     }
     if (speed === "medium") {
-      speedNumber = getRandomInt(3, 6);
+      speedNumber = getRandomFloat(3, 6.5);
     }
     if (speed === "fast") {
-      speedNumber = getRandomInt(1, 2);
+      speedNumber = getRandomFloat(1.5, 2.5);
     }
 
     gradients.push(getGradient(i, palette[Math.floor(Math.random() * palette.length)]));
