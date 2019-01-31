04d59f6b06f36cfef4d3c2cec32668ab0542ccb3
Tim Douglas <tim.douglas@phocassoftware.com>
2019-01-31 11:36:05 +1100

    Removed hold hiding logic for logout button

--- src/components/Toolbar/LogoutButton.js
+++ src/components/Toolbar/LogoutButton.js
@@ -2,18 +2,12 @@ module.exports = function(fastn, app) {
   return fastn(
     "div",
     { class: "logout-button" },
-    fastn(
-      "button",
-      {
-        class: "button",
-        hidden: fastn
-          .binding("login.token", token => token === null || token === "")
-          .attach(app.state)
-      },
-      fastn("span", { class: "title" }, "Logout")
-    ).on("click", event => {
-      event.preventDefault();
-      app.logout();
-    })
+    fastn("button", { class: "button" }, fastn("span", { class: "title" }, "Logout")).on(
+      "click",
+      event => {
+        event.preventDefault();
+        app.logout();
+      }
+    )
   );
 };
