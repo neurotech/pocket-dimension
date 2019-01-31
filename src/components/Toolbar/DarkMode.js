3dd12a63d2781d114bed955d2fdef46a03ed4dcb
Tim Douglas <tim.douglas@phocassoftware.com>
2019-02-01 10:30:00 +1100

    Toolbar CSS fixes

--- src/components/Toolbar/DarkMode.js
+++ src/components/Toolbar/DarkMode.js
@@ -2,15 +2,27 @@ module.exports = function createDarkModeToggle(fastn, app) {
   return fastn(
     "div",
     { class: "dark-mode" },
-    fastn("input", {
-      type: "checkbox",
-      class: "dark-mode-toggle",
-      checked: fastn.binding("theme", theme => {
-        return theme === "dark";
-      })
-    }).on("change", event => {
-      app.setTheme(event.target.checked);
-    }),
-    fastn("label", { class: "dark-mode-label" }, "Dark Mode")
+    fastn(
+      "div",
+      { class: "dark-mode-wrapper" },
+      fastn("input", {
+        type: "checkbox",
+        class: "dark-mode-toggle",
+        checked: fastn.binding("theme", theme => {
+          return theme === "dark";
+        })
+      }).on("change", event => {
+        app.setTheme(event.target.checked);
+      }),
+      fastn(
+        "label",
+        {
+          class: fastn.binding("theme", theme => {
+            return ["dark-mode-label", theme === "dark" && "on"];
+          })
+        },
+        "💡"
+      )
+    )
   );
 };
