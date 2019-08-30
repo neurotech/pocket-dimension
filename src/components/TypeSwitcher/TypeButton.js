04aabc4ca037c10c2a18f07a9ce623aaaf09bf5f
Tim Douglas <tim.douglas@phocassoftware.com>
2019-08-30 10:33:56 +1000

    Automatically generate post title and body when selecting Diary

--- src/components/TypeSwitcher/TypeButton.js
+++ src/components/TypeSwitcher/TypeButton.js
@@ -14,10 +14,17 @@ module.exports = function createTypeButton(fastn, app, title) {
     fastn("span", { class: ["type-switcher-button", title.toLowerCase()] }, title)
   ).on("click", () => {
     app.setPostType(title.toLowerCase());
-    if (title.toLowerCase() === "link") {
-      document.querySelector(".create-post-body").focus();
-    } else {
-      document.querySelector(".create-post-title").focus();
+    switch (title.toLowerCase()) {
+      case "link":
+        document.querySelector(".create-post-body").focus();
+        break;
+      case "diary":
+        app.generatePostTitle();
+        document.querySelector(".create-post-body").focus();
+        break;
+      default:
+        document.querySelector(".create-post-title").focus();
+        break;
     }
   });
 };
