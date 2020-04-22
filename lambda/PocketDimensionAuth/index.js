84b7901dd712aebdbc72c9dc21578c14aa646b70
Tim Douglas <tim.douglas@phocassoftware.com>
2020-04-22 21:07:55 +1000

    Just use one Lambda

--- lambda/PocketDimensionAuth/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-  var response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: {},
-    body: "!",
-  };
-  callback(null, response);
-};
