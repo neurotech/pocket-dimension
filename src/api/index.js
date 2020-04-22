ef8b89a9fd5159c5d5ca459a2edce8e3fb0bdca1
Tim Douglas <tim.douglas@phocassoftware.com>
2020-04-22 18:27:19 +1000

    New URL

--- src/api/index.js
+++ src/api/index.js
@@ -3,28 +3,34 @@ const cpjax = require("cpjax");
 module.exports = {
   get: {
     all: function getAllItems(token, callback) {
-      cpjax({ url: "/api/items/all", auth: `Bearer ${token}` }, function (error, data) {
-        if (error) {
-          return callback(error);
+      cpjax(
+        { url: "https://api.pocket-dimension.space/items/all", auth: `Bearer ${token}` },
+        function (error, data) {
+          if (error) {
+            return callback(error);
+          }
+          let items = JSON.parse(data).data;
+          callback(null, items);
         }
-        let items = JSON.parse(data).data;
-        callback(null, items);
-      });
+      );
     },
     archived: function getAllArchivedItems(token, callback) {
-      cpjax({ url: "/api/items/archived", auth: `Bearer ${token}` }, function (error, data) {
-        if (error) {
-          return callback(error);
+      cpjax(
+        { url: "https://api.pocket-dimension.space/items/archived", auth: `Bearer ${token}` },
+        function (error, data) {
+          if (error) {
+            return callback(error);
+          }
+          let items = JSON.parse(data).data;
+          callback(null, items);
         }
-        let items = JSON.parse(data).data;
-        callback(null, items);
-      });
+      );
     },
   },
   update: function updateItem(token, item, callback) {
     cpjax(
       {
-        url: `/api/item/update/?id=${item.id}&timestamp=${item.timestamp}`,
+        url: `https://api.pocket-dimension.space/item/update/?id=${item.id}&timestamp=${item.timestamp}`,
         method: "PUT",
         json: true,
         auth: `Bearer ${token}`,
@@ -42,7 +48,7 @@ module.exports = {
   create: function postItem(token, item, callback) {
     cpjax(
       {
-        url: "/api/items/create",
+        url: "https://api.pocket-dimension.space/items/create",
         method: "POST",
         auth: `Bearer ${token}`,
         json: true,
@@ -61,7 +67,7 @@ module.exports = {
     item: function deleteItem(token, id, timestamp, callback) {
       cpjax(
         {
-          url: `/api/items/delete/?id=${id}&timestamp=${timestamp}`,
+          url: `https://api.pocket-dimension.space/items/delete/?id=${id}&timestamp=${timestamp}`,
           method: "DELETE",
           auth: `Bearer ${token}`,
           json: true,
@@ -79,7 +85,7 @@ module.exports = {
   pageInfo: function getPageInfo(token, url, callback) {
     cpjax(
       {
-        url: `/api/get-page-info`,
+        url: `https://api.pocket-dimension.space/get-page-info`,
         method: "POST",
         auth: `Bearer ${token}`,
         json: true,
@@ -97,7 +103,12 @@ module.exports = {
   login: function postLogin(login, callback) {
     var credentials = { username: login.username, password: login.password };
     cpjax(
-      { url: "/api/login", method: "POST", json: true, data: JSON.stringify(credentials) },
+      {
+        url: "https://api.pocket-dimension.space/login",
+        method: "POST",
+        json: true,
+        data: JSON.stringify(credentials),
+      },
       function (error, data) {
         if (error) {
           return callback(error);
