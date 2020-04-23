0a214b64808a5460f87b36225c9320703a94c680
Tim Douglas <tim.douglas@phocassoftware.com>
2020-04-23 13:17:43 +1000

    How good is AWS

--- lambda/auth/token.js
+++ lambda/auth/token.js
@@ -4,11 +4,13 @@ const uuid = require("../util/uuid.js");
 const dynamo = require("../dynamo.js");
 const getParameters = require("../util/getParameters.js");
 
-const parameters = righto(getParameters);
-const key = parameters.get((parameters) => {
-  return parameters.POCKET_DIMENSION_SECRET_KEY;
-});
-const getCryptr = key.get((key) => new Cryptr(key));
+function getCryptr() {
+  const parameters = righto(getParameters);
+  const key = parameters.get((parameters) => {
+    return parameters.POCKET_DIMENSION_SECRET_KEY;
+  });
+  return key.get((key) => new Cryptr(key));
+}
 
 function encrypt(cryptr, value, callback) {
   return callback(null, cryptr.encrypt(value));
@@ -29,13 +31,13 @@ module.exports = {
     // Store supplied username and NEW TOKEN in pocket-dimension-auth
     var tokenStored = righto(dynamo.storeToken, { username: username, token: token });
     // Use cryptr.encrypt to encrypt the NEW TOKEN
-    var tokenHash = righto.sync(encrypt, getCryptr, token, righto.after(tokenStored));
+    var tokenHash = righto.sync(encrypt, getCryptr(), token, righto.after(tokenStored));
     // Callback error, results
     tokenHash(callback);
   },
   check: function checkToken(sessionToken, callback) {
     // Attempt to use cryptr to decrypt SESSION TOKEN, return error
-    var token = righto(decrypt, getCryptr, sessionToken);
+    var token = righto(decrypt, getCryptr(), sessionToken);
     // Lookup pocket-dimension-auth table for any records matching DECRYPTED TOKEN
     var user = righto(dynamo.getUserByToken, token);
 
@@ -46,7 +48,7 @@ module.exports = {
     result(callback);
   },
   decrypt: function decryptToken(sessionToken, callback) {
-    var decrypted = righto(decrypt, getCryptr, sessionToken);
+    var decrypted = righto(decrypt, getCryptr(), sessionToken);
     decrypted(callback);
   },
   getTokenFromHeaders: function getToken(headers) {
--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,36 @@
 const auth = require("./auth/index.js");
 const db = require("./dynamo.js");
-//const uuid = require("./util/uuid.js");
+const uuid = require("./util/uuid.js");
+
+function constructItem(payload) {
+  let id = payload.id || uuid();
+  let timestamp = payload.timestamp || new Date().toISOString();
+
+  return {
+    id: id,
+    userId: payload.userId,
+    timestamp: timestamp,
+    title: payload.title,
+    body: payload.body,
+    type: payload.type || "note",
+    generateTitle: payload.generateTitle || false,
+    isArchived: false,
+  };
+}
+
+function createItem(body, callback) {
+  var item = constructItem(JSON.parse(body));
+  db.create(item, (err) => {
+    if (err) return callback(err);
+    var response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(item),
+    };
+    callback(null, response);
+  });
+}
 
 function getItems(userId, archived, callback) {
   db.get(userId.toString(), archived, (error, results) => {
@@ -15,12 +45,37 @@ function getItems(userId, archived, callback) {
   });
 }
 
-exports.handler = (event, context, callback) => {
-  console.log("EVENT: " + JSON.stringify(event));
+function updateItem(id, timestamp, item, callback) {
+  db.update(id, timestamp, item, (error, results) => {
+    if (error) return callback(error);
 
+    var response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(results),
+    };
+    callback(null, response);
+  });
+}
+
+function deleteItem(id, timestamp, callback) {
+  db.remove(id, timestamp, (error, results) => {
+    if (error) return callback(error);
+
+    var response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(results),
+    };
+    callback(null, response);
+  });
+}
+
+exports.handler = (event, context, callback) => {
   // Authenticate
   auth.validateSessionToken(event, (error, user) => {
-    console.warn("VALIDATE WAS CALLED");
     if (error) {
       var errorResponse = {
         isBase64Encoded: false,
@@ -32,131 +87,54 @@ exports.handler = (event, context, callback) => {
     }
     // Token is valid, continue to routing
 
-    // Retrieve Items
+    // Create an Item
+    if (event.path === "/items/create") {
+      if (event.httpMethod === "POST") {
+        return createItem(event.body, callback);
+      }
+    }
+
+    // Retrieve All Items
     if (event.path === "/items/all") {
       if (event.httpMethod === "GET") {
-        getItems(user.userId, false, (error, results) => {
-          if (error) {
-            var errorResponse = {
-              isBase64Encoded: false,
-              statusCode: 500,
-              headers: {},
-              body: "ERROR GETTING ITEMS :" + JSON.stringify(error),
-            };
-            return callback(errorResponse);
-          }
-          console.warn("GOT ITEMS");
-          var successResponse = {
-            isBase64Encoded: false,
-            statusCode: 200,
-            headers: {},
-            body: JSON.stringify(results),
-          };
-          return callback(null, successResponse);
-        });
+        getItems(user.userId, false, callback);
       }
-    } else {
-      console.warn("VALIDATE WAS NOT CALLED");
+    }
+
+    // Retrieve Archived Items
+    if (event.path === "/items/archived") {
+      if (event.httpMethod === "GET") {
+        return getItems(user.userId, true, callback);
+      }
+    }
+
+    // Update Item
+    if (event.path === "/item/update") {
+      if (event.httpMethod === "PUT") {
+        if (event.queryStringParameters.id && event.queryStringParameters.timestamp) {
+          return updateItem(
+            event.queryStringParameters.id,
+            event.queryStringParameters.timestamp,
+            JSON.parse(event.body),
+            callback
+          );
+        }
+      }
+    }
+
+    // Delete Item
+    if (event.path === "/item/delete") {
+      if (event.httpMethod === "DELETE") {
+        if (event.queryStringParameters.id && event.queryStringParameters.timestamp) {
+          return deleteItem(
+            event.queryStringParameters.id,
+            event.queryStringParameters.timestamp,
+            callback
+          );
+        } else {
+          return callback("Missing ID or timestamp!");
+        }
+      }
     }
   });
 };
-
-// function constructItem(payload) {
-//   let id = payload.id || uuid();
-//   let timestamp = payload.timestamp || new Date().toISOString();
-
-//   return {
-//     id: id,
-//     userId: payload.userId,
-//     timestamp: timestamp,
-//     title: payload.title,
-//     body: payload.body,
-//     type: payload.type || "note",
-//     generateTitle: payload.generateTitle || false,
-//     isArchived: false,
-//   };
-// }
-
-// function createItem(body, callback) {
-//   var item = constructItem(JSON.parse(body));
-//   db.create(item, (err) => {
-//     if (err) return callback(err);
-//     var response = {
-//       isBase64Encoded: false,
-//       statusCode: 200,
-//       headers: {},
-//       body: JSON.stringify(item),
-//     };
-//     callback(null, response);
-//   });
-// }
-
-// function updateItem(id, timestamp, item, callback) {
-//   db.update(id, timestamp, item, (error, results) => {
-//     if (error) return callback(error);
-
-//     var response = {
-//       isBase64Encoded: false,
-//       statusCode: 200,
-//       headers: {},
-//       body: JSON.stringify(results),
-//     };
-//     callback(null, response);
-//   });
-// }
-
-// function deleteItem(id, timestamp, callback) {
-//   db.remove(id, timestamp, (error, results) => {
-//     if (error) return callback(error);
-
-//     var response = {
-//       isBase64Encoded: false,
-//       statusCode: 200,
-//       headers: {},
-//       body: JSON.stringify(results),
-//     };
-//     callback(null, response);
-//   });
-// }
-
-// // Create an Item
-// if (event.path === "/items/create") {
-//   if (event.httpMethod === "POST") {
-//     return createItem(event.body, callback);
-//   }
-// }
-
-// if (event.path === "/items/archived") {
-//   if (event.httpMethod === "GET") {
-//     return getItems(user.userId, true, callback);
-//   }
-// }
-
-// // Update Item
-// if (event.path === "/item/update") {
-//   if (event.httpMethod === "PUT") {
-//     if (event.queryStringParameters.id && event.queryStringParameters.timestamp) {
-//       return updateItem(
-//         event.queryStringParameters.id,
-//         event.queryStringParameters.timestamp,
-//         JSON.parse(event.body),
-//         callback
-//       );
-//     }
-//   }
-// }
-
-// // Delete Item
-// if (event.path === "/item/delete") {
-//   if (event.httpMethod === "DELETE") {
-//     if (event.queryStringParameters.id && event.queryStringParameters.timestamp) {
-//       return deleteItem(
-//         event.queryStringParameters.id,
-//         event.queryStringParameters.timestamp,
-//         callback
-//       );
-//     } else {
-//       return callback("Missing ID or timestamp!");
-//     }
-//   }
-// }
