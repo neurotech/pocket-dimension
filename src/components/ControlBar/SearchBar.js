8a870465aefaa364015c59f4fab64104c76557cf
Tim Douglas <tim.douglas@phocassoftware.com>
2020-07-13 10:00:50 +1000

    Added clear button to searchbar

--- src/components/ControlBar/SearchBar.js
+++ src/components/ControlBar/SearchBar.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FILTER_TEXT_CHANGED } from "../../util/actionTypes";
 import { useStore } from "../../util/Store";
+import TextButton from "../ui/TextButton";
 
 const StyledInput = styled.input`
   outline: 0;
   width: 100%;
   padding: ${({ theme }) => theme.searchBarPadding};
-  border-radius: ${({ theme }) => theme.borderRadius};
+  border-top-left-radius: ${({ theme }) => theme.borderRadius};
+  border-top-right-radius: 0;
+  border-bottom-left-radius: ${({ theme }) => theme.borderRadius};
+  border-bottom-right-radius: 0;
 
   border: 2px solid ${({ theme }) => theme.palette.searchBarBorder};
   background-color: ${({ theme }) => theme.palette.searchBarBackground};
@@ -31,23 +35,76 @@ const StyledInput = styled.input`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const StyledTextButton = styled(TextButton)`
+  border-top-left-radius: 0;
+  border-top-right-radius: ${({ theme }) => theme.borderRadius};
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: ${({ theme }) => theme.borderRadius};
+  margin-left: -${({ theme }) => theme.borderWidth}px;
+
+  border: 2px solid
+    ${({ theme, searchBarFocus }) =>
+      searchBarFocus
+        ? theme.palette.clearSearchBarBorderFocus
+        : theme.palette.clearSearchBarBorder};
+  background-color: ${({ theme, searchBarFocus }) =>
+    searchBarFocus
+      ? theme.palette.clearSearchBarBackgroundFocus
+      : theme.palette.clearSearchBarBackground};
+  color: ${({ theme, searchBarFocus }) =>
+    searchBarFocus
+      ? theme.palette.clearSearchBarPlaceholderFocus
+      : theme.palette.clearSearchBarPlaceholder};
+
+  &:hover {
+    background-color: ${({ theme, searchBarFocus }) =>
+      searchBarFocus
+        ? theme.palette.clearSearchBarBackgroundHover
+        : theme.palette.clearSearchBarBackground};
+
+    color: ${({ theme, searchBarFocus }) =>
+      searchBarFocus
+        ? theme.palette.clearSearchBarHover
+        : theme.palette.clearSearchBarPlaceholder};
+  }
+`;
+
 const SearchBar = () => {
   const { state, dispatch } = useStore();
+  const [searchBarFocus, setSearchBarFocus] = useState(false);
 
   return (
-    <StyledInput
-      autoFocus
-      autocomplete={"off"}
-      onChange={(event) => {
-        dispatch({
-          type: FILTER_TEXT_CHANGED,
-          payload: event ? event.target.value : "",
-        });
-      }}
-      placeholder="Search"
-      type="search"
-      value={state.filterText || ""}
-    ></StyledInput>
+    <Container>
+      <StyledInput
+        autocomplete={"off"}
+        onChange={(event) => {
+          dispatch({
+            type: FILTER_TEXT_CHANGED,
+            payload: event ? event.target.value : "",
+          });
+        }}
+        placeholder="Search"
+        value={state.filterText || ""}
+        onFocus={() => setSearchBarFocus(true)}
+        onBlur={() => setSearchBarFocus(false)}
+      ></StyledInput>
+      <StyledTextButton
+        size="large"
+        label="Clear"
+        searchBarFocus={searchBarFocus}
+        onClick={() => {
+          dispatch({
+            type: FILTER_TEXT_CHANGED,
+            payload: "",
+          });
+        }}
+      />
+    </Container>
   );
 };
 
