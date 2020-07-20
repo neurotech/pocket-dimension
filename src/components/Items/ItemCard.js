b90920082aeda43e5713eb0dee3418377729fe49
Tim Douglas <tim.douglas@phocassoftware.com>
2020-07-20 16:20:24 +1000

    Updated ItemCard styling

--- src/components/Items/ItemCard.js
+++ src/components/Items/ItemCard.js
@@ -1,10 +1,37 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const resolveTypeToAccentColour = (theme, itemType) => {
+  switch (itemType) {
+    case "note":
+      return theme.commonPalette.blue;
+
+    case "link":
+      return theme.commonPalette.green;
+
+    case "diary":
+      return theme.commonPalette.yellow;
+  }
+};
+
+const generateGradientMixinForType = (itemType) => {
+  let mixin = css`
+    background: linear-gradient(
+      90deg,
+      ${({ theme }) => resolveTypeToAccentColour(theme, itemType)} 0%,
+      ${({ theme }) => resolveTypeToAccentColour(theme, itemType)} 4px,
+      ${({ theme }) => theme.palette.boxBackground} 4px,
+      ${({ theme }) => theme.palette.boxBackground} 100%
+    );
+  `;
+
+  return mixin;
+};
 
 const StyledItemCard = styled.div`
+  background: ${({ theme }) => theme.palette.boxBackground};
   border-radius: ${({ theme }) => theme.borderRadius};
   border: 2px solid ${({ theme }) => theme.palette.boxBorder};
-  background-color: ${({ theme }) => theme.palette.boxBackground};
   color: ${({ theme }) => theme.palette.text};
 
   padding: ${({ theme }) => theme.cardPadding};
@@ -12,10 +39,18 @@ const StyledItemCard = styled.div`
   filter: grayscale(${(props) => (props.isStale ? 1 : 0)});
   opacity: ${(props) => (props.isStale ? 0.5 : 1)};
   cursor: ${(props) => (props.isStale ? "not-allowed" : "auto")};
+
+  & :hover {
+    ${(props) => generateGradientMixinForType(props.itemType)};
+  }
 `;
 
-const ItemCard = ({ isStale = false, children }) => {
-  return <StyledItemCard isStale={isStale}>{children}</StyledItemCard>;
+const ItemCard = ({ isStale = false, title, itemType = "note", children }) => {
+  return (
+    <StyledItemCard title={title} itemType={itemType} isStale={isStale}>
+      {children}
+    </StyledItemCard>
+  );
 };
 
 export default ItemCard;
